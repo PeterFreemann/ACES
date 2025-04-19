957b18f451dc72f3bec1a333193851a844ba808b
PeterFreemann <128150016+PeterFreemann@users.noreply.github.com>
2025-04-19 13:06:53 +0100

    changes

--- src/component/WomeShoppingPage.jsx
+++ src/component/WomeShoppingPage.jsx
@@ -4,231 +4,75 @@ const cards = [
   {
     id: 1,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/lady.jpg",
+    price: "$2000",
+    image: "/images/imagee.jpg",
   },
   {
     id: 2,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$4000",
-    image: "/images/ladyy.jpg",
+    price: "$2000",
+    image: "/images/imageeee.jpg",
   },
   {
     id: 3,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$5000",
-    image: "/images/ladyyyyy.jpg",
+    price: "$2000",
+    image: "/images/imageee.jpg",
   },
   {
     id: 4,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$6000",
-    image: "/images/ladyyyyyy.jpg",
+    price: "$2000",
+    image: "/images/imageeeee.jpg",
   },
   {
     id: 5,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$7000",
-    image: "/images/ladyyyyyy.jpg",
+    price: "$2000",
+    image: "/images/imageeeeee.jpg",
   },
   {
     id: 6,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$8000",
-    image: "/images/ladyyyyyyy.jpg",
+    price: "$2000",
+    image: "/images/imageeeeeee.jpg",
   },
   {
     id: 7,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$9000",
-    image: "/images/ladyyyyyyyy.jpg",
+    price: "$2000",
+    image: "/images/imageeeeeeee.jpg",
   },
   {
     id: 8,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$10000",
-    image: "/images/ladyyyyyyyyy.jpg",
+    price: "$2000",
+    image: "/images/imageeeeeeeee.jpg",
   },
   {
     id: 9,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$11000",
-    image: "/images/ladyyyyyyyyyy.jpg",
+    price: "$2000",
+    image: "/images/imageeeeeeeeee.jpg",
   },
   {
     id: 10,
     name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "12000",
-    image: "/images/ladyyyyyyyyyyy.jpg",
-  },
-  {
-    id: 11,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$13000",
-    image: "/images/ladyyyyyyyyyyyyy.jpg",
-  },
-  {
-    id: 12,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$14000",
-    image: "/images/ladyyyyyyyyyyyyyyy.jpg",
-  },
-  {
-    id: 13,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$15000",
-    image: "/images/ladyyyyyyyyyyyyyyyyy.jpg",
-  },
-  {
-    id: 14,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/ladyyyyyyyyyyyyyyyyy.jpg",
-  },
-  {
-    id: 15,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 16,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 17,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 18,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 19,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 20,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 21,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 22,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 23,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 24,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 25,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 26,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 27,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 28,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 29,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 30,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 31,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 32,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 33,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 34,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 35,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
-  },
-  {
-    id: 36,
-    name: "ACES DESIGN oversized striped t-shirt with sports graphic in black",
-    price: "$3000",
-    image: "/images/womenimage.jpg",
+    price: "$2000",
+    image: "/images/imageeeeeeeeeee.jpg",
   },
 ];
 
-export default function WomenShoppingPage() {
+export default function MenShoppingPage() {
   const navigate = useNavigate();
 
   return (
     <section className="relative top-[32px] mb-[70px]">
       <div className="mt-12 py-4 bg-black text-white font-bold font-serif text-center">
-        THE UP-TO-30%-OFF EDIT
+        THE MEN'S DROP
       </div>
       <div className="py-4 bg-white text-black font-bold font-serif text-center">
-        Fits
+        Men’s Fits
       </div>
 
       <div className="grid grid-cols-2 gap-2 px-4 sm:px-12 md:grid-cols-3 lg:grid-cols-4">
@@ -249,7 +93,7 @@ export default function WomenShoppingPage() {
                   <button
                     className="btn bg-gray-700 px-2 py-1 text-xs text-white font-semibold"
                     onClick={() =>
-                      navigate("/product-details", { state: { product } })
+                      navigate("/menproduct-details", { state: { product } })
                     }
                   >
                     SELLING FAST
